cd5ef3754309c71917c21d280371260ae6400b7f
igibek <koishybayev.igibek@gmail.com>
2022-11-08 11:10:47 -0500

    changed the custom bash script directory

--- src/Runner.Worker/Handlers/ScriptHandler.cs
+++ src/Runner.Worker/Handlers/ScriptHandler.cs
@@ -327,19 +327,17 @@ namespace GitHub.Runner.Worker.Handlers
                 StepHost.OutputDataReceived += stdoutManager.OnDataReceived;
                 StepHost.ErrorDataReceived += stderrManager.OnDataReceived;
 
-                // TODO: generate the script for the bash taint tracker
+                // HACK: generate the script for the bash taint tracker
                 if (ExecutionContext.TaintContext.Inputs.TryGetValue("script", out TaintVariable _var)) {
                     string content = _var.EvaluatedValue;
                     foreach (var val in ExecutionContext.TaintContext.Root.Values) {
                         content = content.Replace(val, "$TAINTED_VALUE");
                     }
-                    string pluginScriptPath = Path.Combine(TaintContext.RepositoryDirectory, "tainted_" + Path.GetFileName(scriptFilePath));
+                    // TODO: fix the plugin script path to be inside the temp directory
+                    string pluginScriptPath = Path.Combine(Path.GetDirectoryName(scriptFilePath), "tainted_" + Path.GetFileName(scriptFilePath));
                     File.WriteAllText(pluginScriptPath, content);
-                    int moduleExitCode = await ExecutionContext.TaintContext.ExecutePlugin(Data.ExecutionType, ActionDirectory ?? pluginScriptPath);
+                    int pluginExitCode = await ExecutionContext.TaintContext.ExecutePlugin(Data.ExecutionType, ActionDirectory ?? pluginScriptPath);
                 }
-
-                
-                
 
                 // Execute
                 int exitCode = await StepHost.ExecuteAsync(ExecutionContext,
